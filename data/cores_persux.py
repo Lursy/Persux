05f9e5a24ca2dc7399d1a5c7d2927da0e771ded2
Lursy <70035797+Lursy@users.noreply.github.com>
2021-09-13 18:46:40 -0300

    Create cores_persux.py

---
+++ data/cores_persux.py
@@ -0,0 +1,23 @@
+# letras
+ve = '\033[1;31m'  # Vermelho
+vd = '\033[1;32m'  # Verde
+am = '\033[1;33m'  # Amarelo
+az = '\033[1;34m'  # Azul
+br = '\033[1;37m'  # Branco
+cy = '\033[1;36m'  # Ciano
+rx = '\033[0;35m'  # Roxo
+pt = '\033[1;30m'  # Preto
+
+
+# fundos
+fve = '\033[1;41m'  # Vermelho
+fvd = '\033[1;42m'  # Verde
+fam = '\033[1;43m'  # Amarelo
+faz = '\033[1;44m'  # Azul
+fbr = '\033[1;47m'  # Branco
+fcy = '\033[1;46m'  # Ciano
+frx = '\033[0;45m'  # Roxo
+
+
+# Padrão
+pb = '\033[0m'
