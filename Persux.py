0d65b0f59d8da2b26c5909947f5490fd52526661
gabrielfelipe <gabrielfelipeassuncaodesouza@gmail.com>
2023-05-12 16:42:49 -0300

    Refatorar funcao que faz os imports

--- Persux.py
+++ Persux.py
@@ -1,11 +1,9 @@
 from data.install import *
 
-
 git_pull()
 os.chdir('/data/data/com.termux/files/usr/etc/')
 usr = os.path.exists('.usuario')
 install()
-
 
 try:
    while True:
@@ -14,6 +12,10 @@ try:
         from data.ferramentas import clear
         clear()
         if not usr:
+            """
+            In this case the user haven't run the script anytime yet
+            """
+
             print(f'{cy}~Lursy: {vd}Olá...')
             sleep(1)
             print('        É sua primeira vez por aqui?')
@@ -43,32 +45,54 @@ Me ajude a fazer novos scripts apoiando no PIX
             print(f'{cy}~Lursy: {vd}Boa escolha!')
             sleep(1)
             usr = True
-        if menu == '1':
+
+        """
+        It's implemented a Object Literal rigjt down that has made for remove all unuseful conditionais. It also removes the boring task to change the code in two place when we wanna add a new funcionality in the future
+
+        First we define the principal functions that will toggle among the possible imports, and later, we put them into a dict where for each key there's a lambda function that calls the functions declarated above
+        """
+
+        def importat_letra():
             from data.letra import letra
             letra()
-        elif menu == '2':
+
+        def importar_banner():
             from data.banner import banner
             banner()
-        elif menu == '3':
+
+        def importar_senha():
             from data.senha import senha
             senha()
-        elif menu == '4':
+
+        def importar_nome():
             from data.nome import nome_letra
             nome_letra()
             from data.ferramentas import *
-        elif menu == '5':
+
+        def open_url():
             os.system(f'termux-open-url https://www.youtube.com/channel/UCwmkiKIZHL1wscYHfIINZKw')
             clear()
-        elif menu == '6':
+
+        def sair():
             print(f'\n{vd}Saindo...')
             sleep(1)
             clear()
             break
-        else:
+
+        import_handler = {
+            "1": lambda: importar_letra,
+            "2": lambda: importar_banner,
+            "3": lambda: importar_senha,
+            "4": lambda: importar_nome,
+            "5": lambda: open_url
+                }
+        try:
+            import_handler[menu]()()
+        except KeyError:
             print(f'{ve}Comando não reconhecido')
             sleep(1)
+
 except KeyboardInterrupt:
     print(f'\n{vd}Saindo...')
     sleep(1)
     clear()
-    
